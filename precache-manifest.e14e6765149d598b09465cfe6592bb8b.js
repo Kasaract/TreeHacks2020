2000da283606ee81fdb0a17eca7454d06917fc74
Kasaract <garynguyen2018@gmail.com>
2020-02-15 09:07:04 -0500

    Updates

---
+++ precache-manifest.e14e6765149d598b09465cfe6592bb8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fe1f9fe28fe57fb088eb92e85a1380e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a82eb14c5dbbecb9efe",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c8f2f316bf47f3c9e9da",
+    "url": "/static/js/2.f0c9eb83.chunk.js"
+  },
+  {
+    "revision": "26e903b0715b080a5c2a7ddbdde3ed04",
+    "url": "/static/js/2.f0c9eb83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a82eb14c5dbbecb9efe",
+    "url": "/static/js/main.4f25846b.chunk.js"
+  },
+  {
+    "revision": "3e4bd12635964c43f06d",
+    "url": "/static/js/runtime-main.d2746ff7.js"
+  }
+]);
