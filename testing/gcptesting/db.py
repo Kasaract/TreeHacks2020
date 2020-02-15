ecef33f706d1d5822ae97fab773b9250b7da608f
James <jameswsj@berkeley.edu>
2020-02-14 23:10:43 -0800

    added reference methods for interacting with firebase db with python

--- testing/gcptesting/db.py
+++ testing/gcptesting/db.py
@@ -8,3 +8,33 @@ config = {
 }
 
 firebase = Firebase(config)
+
+#Authentication
+# Get a reference to the auth service
+auth = firebase.auth()
+
+# Log the user in
+user = auth.sign_in_with_email_and_password("jameswsj@berkeley.edu", password)
+
+# Get a reference to the database service
+db = firebase.database()
+
+# data to save
+data = {
+    "name": "JAmes Jung"
+}
+
+# Pass the user's idToken to the push method
+results = db.child("users").push(data, user['idToken'])
+
+# building paths to databse
+db = firebase.database()
+db.child("users").child("James")
+
+
+#retrieving data
+users = db.child("users").get()
+print(users.val())
+""" above function should return
+>>> {"James": {"name": "JAmes Jung"}, "Tarang": {"name": "Tarang Srivastava"}}
+"""
