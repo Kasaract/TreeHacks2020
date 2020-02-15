e3648db504060a928e21ee6e673c8a8067e87b66
Kasaract <garynguyen2018@gmail.com>
2020-02-15 15:03:37 -0800

    Change map directory

--- src/pages/Home/Map/index.js
+++ src/pages/Home/Map/index.js
@@ -9,11 +9,13 @@ import {
 import mapStyles from './mapStyles';
 import * as pointData from './locations.json';
 
-const Map = () => (
+const Map = ({ center, zoom }) => (
 	<GoogleMap
 		defaultZoom={12}
 		defaultCenter={{ lat: 40.754954, lng: -73.995338 }}
 		defaultOptions={{ styles: mapStyles }}
+		center={center}
+		zoom={zoom}
 	>
 		{pointData.spots.map(spot => (
 			<Marker key={spot.id} position={{ lat: spot.lat, lng: spot.lng }} />
@@ -23,13 +25,15 @@ const Map = () => (
 
 const WrappedMap = withScriptjs(withGoogleMap(Map));
 
-const DisplayGoogleMap = () => (
+const DisplayGoogleMap = ({ center, zoom }) => (
 	<div style={{ width: '85vw', height: '50vh' }}>
 		<WrappedMap
 			googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${'AIzaSyC3LxcAOwU1ooKUK4gIwGx91LVuKD3k_0g'}`}
 			loadingElement={<div style={{ height: `100%` }} />}
 			containerElement={<div style={{ height: `100%` }} />}
 			mapElement={<div style={{ height: `100%` }} />}
+			center={center}
+			zoom={zoom}
 		/>
 	</div>
 );
