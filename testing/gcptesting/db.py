63272fd4b169e87bb1ba50c0a3f89d9b3a80962a
Tarang Srivastava <tarang.sriv@berkeley.edu>
2020-02-15 13:17:26 -0800

    modified gcp that updates the db

--- testing/gcptesting/db.py
+++ testing/gcptesting/db.py
@@ -1,40 +1,48 @@
 from firebase import Firebase
+import time
 
 config = {
-  "apiKey": "apiKey",
-  "authDomain": "projectId.firebaseapp.com",
-  "databaseURL": "https://databaseName.firebaseio.com",
-  "storageBucket": "projectId.appspot.com"
+  "apiKey": "AIzaSyCBpDzAUiRY8nyiaNadyUIcjLTJxW41dhU",
+  "authDomain": 'recap-treehacks.firebaseapp.com',
+  "databaseURL": 'https://recap-treehacks.firebaseio.com',
+  "storageBucket": 'recap-treehacks.appspot.com'
 }
 
 firebase = Firebase(config)
 
 #Authentication
 # Get a reference to the auth service
-auth = firebase.auth()
+# auth = firebase.auth()
 
-# Log the user in
-user = auth.sign_in_with_email_and_password("jameswsj@berkeley.edu", password)
+# # Log the user in
+# user = auth.sign_in_with_email_and_password("jameswsj@berkeley.edu", password)
 
 # Get a reference to the database service
 db = firebase.database()
 
 # data to save
+i = 0
 data = {
-    "name": "JAmes Jung"
+    "name": "James Jung" + str(i)
 }
 
 # Pass the user's idToken to the push method
-results = db.child("users").push(data, user['idToken'])
+while True:
+  db.child("articles").push(data)
+  time.sleep(3)
+  i += 1
+  data = {
+      "name": "James Jung" + str(i)
+  }
 
 # building paths to databse
-db = firebase.database()
-db.child("users").child("James")
+# db = firebase.database()
+# db.child("users").child("James")
 
 
-#retrieving data
-users = db.child("users").get()
-print(users.val())
-""" above function should return
->>> {"James": {"name": "JAmes Jung"}, "Tarang": {"name": "Tarang Srivastava"}}
-"""
+# #retrieving data
+# users = db.child("users").get()
+# print(users.val())
+# """ above function should return
+# >>> {"James": {"name": "JAmes Jung"}, "Tarang": {"name": "Tarang Srivastava"}}
+# """
