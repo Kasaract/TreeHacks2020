b15b69dfc8e706babdeb5d86b6e322e2b692cfc5
Tarang Srivastava <tarang.sriv@berkeley.edu>
2020-02-15 19:44:30 -0800

    turned geocode into a class for when that is needed

--- src/services/geocode.js
+++ src/services/geocode.js
@@ -6,19 +6,27 @@ const API_KEY = '&key=AIzaSyAg3ugG7gGUMmRz09iKdgUOWHzjKusyu-I';
 const GEOCODE_URL = 'https://maps.googleapis.com/maps/api/geocode/json?address='
  
 
-function get(url){
-    var Httpreq = new XMLHttpRequest();
-    Httpreq.open("GET",url,false);
-    Httpreq.send(null);
-    return Httpreq.responseText;          
+class Geocode {
+
+    constructor() {}
+
+    Get(url){
+        var Httpreq = new XMLHttpRequest();
+        Httpreq.open("GET",url,false);
+        Httpreq.send(null);
+        return Httpreq.responseText;          
+    }
+
+    cityLatitude(city) {
+        let json = JSON.parse(this.Get(GEOCODE_URL+city+API_KEY));
+        return json.results[0].geometry.location.lat;
+    }
+
+    cityLongitude(city) {
+        let json = JSON.parse(this.Get(GEOCODE_URL + city + API_KEY));
+        return json.results[0].geometry.location.lng;
+    }
+
 }
 
-function cityLatitude(city) {
-    let json = JSON.parse(get(GEOCODE_URL+city+API_KEY));
-    return json.results[0].geometry.location.lat;
-}
-
-function cityLongitude(city) {
-    let json = JSON.parse(get(GEOCODE_URL + city + API_KEY));
-    return json.results[0].geometry.location.lng;
-}
+export default new Geocode();
