1f4e2751fc1d8208169d04d29359d169a96650ce
Tarang Srivastava <tarang.sriv@berkeley.edu>
2020-02-15 16:43:23 -0800

    geocode file to get states lat and long

--- src/services/geocode.js
+++ src/services/geocode.js
@@ -2,11 +2,25 @@
  * Call the functions in here to use Google's Geocode API
  */
 
-const API_KEY = 'AIzaSyAg3ugG7gGUMmRz09iKdgUOWHzjKusyu-I';
+const API_KEY = '&keyAIzaSyAg3ugG7gGUMmRz09iKdgUOWHzjKusyu-I';
+const GEOCODE_URL = 'https://maps.googleapis.com/maps/api/geocode/json?address='
+ 
 
-function cityLatitude(city) {
-
+function get(url){
+    var Httpreq = new XMLHttpRequest();
+    Httpreq.open("GET",url,false);
+    Httpreq.send(null);
+    return Httpreq.responseText;          
 }
 
+function cityLatitude(city) {
+    let json = JSON.parse(get(GEOCODE_URL+city+API_KEY));
+    return json.results.location.lat
+}
+
+function cityLongitude(city) {
+    let json = JSON.parse(get(GEOCODE_URL+city+API_KEY));
+    return json.results.location.long
+}
 
 
