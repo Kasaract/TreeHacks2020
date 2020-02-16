e1405a64b711dc03c73ef5dd9d25c9e5f47dc835
Tarang Srivastava <tarang.sriv@berkeley.edu>
2020-02-15 17:06:49 -0800

    gixed geocode.js

--- src/services/geocode.js
+++ src/services/geocode.js
@@ -2,7 +2,7 @@
  * Call the functions in here to use Google's Geocode API
  */
 
-const API_KEY = '&keyAIzaSyAg3ugG7gGUMmRz09iKdgUOWHzjKusyu-I';
+const API_KEY = '&key=AIzaSyAg3ugG7gGUMmRz09iKdgUOWHzjKusyu-I';
 const GEOCODE_URL = 'https://maps.googleapis.com/maps/api/geocode/json?address='
  
 
@@ -15,12 +15,10 @@ function get(url){
 
 function cityLatitude(city) {
     let json = JSON.parse(get(GEOCODE_URL+city+API_KEY));
-    return json.results.location.lat
+    return json.results[0].geometry.location.lat;
 }
 
 function cityLongitude(city) {
-    let json = JSON.parse(get(GEOCODE_URL+city+API_KEY));
-    return json.results.location.long
+    let json = JSON.parse(get(GEOCODE_URL + city + API_KEY));
+    return json.results[0].geometry.location.lng;
 }
-
-
